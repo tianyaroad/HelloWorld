aeffe5a7387f046d7c53b88cf79893216cdd1d32
zhangjian <2424542826@qq.com>
2018-07-22 10:31:54 +0800

    第一个helloworld

---
+++ src/test.java
@@ -0,0 +1,18 @@
+public class test {
+    private static final String KEY_ = "";
+    private static final String TAG = "test";
+
+    public static String getKey() {
+        return KEY_;
+    }
+
+    public static String getTAG() {
+        return TAG;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("dsafjks");
+    }
+
+}
